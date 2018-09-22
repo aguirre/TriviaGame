e2371f0288d8f1765ce4f11d8f7499d9634b58b9
thirteen <41817587+maguirre13@users.noreply.github.com>
2018-09-22 01:17:35 -0700

    update app

---
+++ assets/javascript/app.js
@@ -0,0 +1,58 @@
+var questions = [
+  {
+    question: "What is the name of the Super Mario Bros. sidekick dinosaur?",
+    answer1: "Birdo",
+    answer2: "Yoshi",
+    answer3: "Dino",
+    answer4: "Yoda",
+    correct: "Yoshi"
+  },
+  {
+    question: "What is the name of the castle in Zelda?",
+    answer1: "Helm's Deep",
+    answer2: "Hyrule",
+    answer3: "Riverrun",
+    answer4: "Baron",
+    correct: "Hyrule"
+  },
+  {
+    question: "Who is the main antagonist in Crash Bandicoot?",
+    answer1: "Doctor Robotnik",
+    answer2: "Doctor Claw",
+    answer3: "Doctor Cortex",
+    answer4: "Doctor Evil",
+    correct: "Doctor Cortex"
+  },
+  {
+    question: "What is Sonic the Hedgehog's favorite food?",
+    answer1: "Pizza",
+    answer2: "Nachos",
+    answer3: "Chili Dog",
+    answer4: "Hamburger",
+    correct: "Chili Dog"
+  },
+  {
+    question: "Who is the main character in Half-Life?",
+    answer1: "Adrian Shephard",
+    answer2: "Gordon Freeman",
+    answer3: "Richard Keller",
+    answer4: "Barney Calhoun",
+    correct: "Gordon Freeman"
+  },
+  {
+    question: "What is the name of the main AI in Halo?",
+    answer1: "Siri",
+    answer2: "Cortana",
+    answer3: "Alexa",
+    answer4: "Serina",
+    correct: "Cortana"
+  },
+  {
+    question: "What fruit in PacMan is worth the most points?",
+    answer1: "Melon",
+    answer2: "Orange",
+    answer3: "Cherry",
+    answer4: "Apple",
+    correct: "Melon"
+  }
+];
